0fdc216f61ee9d5d38978d8735b840546b36a5ac
thiagosouza28 <thgdsztls@gmail.>
2025-11-08 15:14:44 -0300

    melhorias no gerenciamneto de eventos e inscrições

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,3 +1,3 @@
-const DEFAULT_API_URL = "http://localhost:3001/api";
+const DEFAULT_API_URL = "https://23b3e5fee9f4.ngrok-free.app/api";
 export const API_BASE_URL = import.meta.env.VITE_API_URL ?? DEFAULT_API_URL;
 //# sourceMappingURL=api.js.map
