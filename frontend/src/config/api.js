d7aaa6ae375911bb76dcaf3cccbf301e50957b64
thiagosouza28 <thgdsztls@gmail.>
2025-11-11 18:42:25 -0300

    Atualização do vercel.json pra deploy

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,3 +1,3 @@
-const DEFAULT_API_URL = "http://localhost:3001/api";
+const DEFAULT_API_URL = "http://eventos-ipitinga-api-git-main-thiago-souzas-projects-8e591c8d.vercel.app/api";
 export const API_BASE_URL = import.meta.env.VITE_API_URL ?? DEFAULT_API_URL;
 //# sourceMappingURL=api.js.map
